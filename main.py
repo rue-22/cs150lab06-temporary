21ecdd87969c3795170d42c53a6cf813171ad2c6
SamTeng22 <125368224+SamTeng22@users.noreply.github.com>
2024-10-12 22:13:53 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 from required_types import PlayerId, HandId, Action, HandInfo
 from view import ChopsticksTerminalView
-from model import ChopsticksGameModel
+from model import ChopsticksGameModel, Hand
 import sys
+from collections.abc import Sequence
 
 class ChopsticksGameController:
     def __init__(self, n: int, k: int, m: int, model: ChopsticksGameModel, view: ChopsticksTerminalView):
@@ -14,6 +15,49 @@ class ChopsticksGameController:
     def start(self):
         model = self._model
         view = self._view
+
+        playerslist = model.initialize_players()
+        round = 1
+        indexlist = [i for i in range(len(playerslist))]
+        nextindex = indexlist[0]
+        current : PlayerId = playerslist[nextindex].player_id
+        playerandhand : dict[PlayerId, list[Hand]] = {}
+
+        for player in playerslist:
+            currid = player.player_id
+            playerandhand[currid] = player.player_hands
+        
+        while True:
+            current = playerslist[nextindex].player_id
+            view.clear_screen()
+            view.show_round_number(round)
+            view.show_all_hands(playerandhand, current)
+
+            action = view.ask_for_action()
+
+            match action:
+                case Action.TAP:
+                    for currplayer in playerslist:
+                        if currplayer.player_id == current:
+                            sourceplayer = currplayer
+                    sources = sourceplayer.player_hands
+                    otherhands = []
+                    for otherplayer in playerslist:
+                        if otherplayer.player_id != current:
+                            for hand in otherplayer.player_hands:
+                                otherhands.append(hand)
+                    targets = otherhands
+                    source, target = view.ask_for_tap_pair(sources, targets)
+                    model.perform_tap(source.fingers_up, source, target)
+                case Action.SPLIT:
+                    pass
+            
+            round+=1
+            
+            nextindex = nextindex + 1 if nextindex + 1 in indexlist else indexlist[0] 
+            if round == 5:
+                break
+
 
         print('here at start')
 
@@ -34,3 +78,4 @@ def main():
 
 if __name__ == "__main__":
     main()
+
